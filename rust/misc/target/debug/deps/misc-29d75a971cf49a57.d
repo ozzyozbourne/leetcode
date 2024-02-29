da975ca96bf80269d95af253cb5cecb64d639280
ozzyozbourne <khanosaid726@gmail.com>
2024-02-28 23:47:06 -0500

    updated gitignore

--- rust/misc/target/debug/deps/misc-29d75a971cf49a57.d
+++
@@ -1,5 +0,0 @@
-/home/ozzy/leetcode/rust/misc/target/debug/deps/libmisc-29d75a971cf49a57.rmeta: src/lib.rs
-
-/home/ozzy/leetcode/rust/misc/target/debug/deps/misc-29d75a971cf49a57.d: src/lib.rs
-
-src/lib.rs:
--- rust/misc/target/debug/deps/misc-8d72afc884786f13.d
+++
@@ -1,7 +0,0 @@
-/home/ozzy/leetcode/rust/misc/target/debug/deps/libmisc-8d72afc884786f13.rmeta: src/lib.rs
-
-/home/ozzy/leetcode/rust/misc/target/debug/deps/libmisc-8d72afc884786f13.rlib: src/lib.rs
-
-/home/ozzy/leetcode/rust/misc/target/debug/deps/misc-8d72afc884786f13.d: src/lib.rs
-
-src/lib.rs:
