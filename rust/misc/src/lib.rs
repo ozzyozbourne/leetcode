92c4b8b6cabb271fed84caa448e396ea35075d9e
ozzyozbourne <khanosaid726@gmail.com>
2024-02-28 23:35:59 -0500

    stucture added for leetcode

---
+++ rust/misc/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub fn tail_recursive_factorial(n: i32, r: i32) -> i32 {
+    if n <= 1 {
+        return r;
+    }
+    tail_recursive_factorial(n - 1, n * r)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works_one() {
+        assert_eq!(tail_recursive_factorial(5, 1), 120);
+        assert_eq!(tail_recursive_factorial(4, 1), 24);
+        assert_eq!(tail_recursive_factorial(3, 1), 6);
+    }
+}
