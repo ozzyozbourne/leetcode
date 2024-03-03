3ffc24e0eb5db29349c3deea945200b7de960c4c
ozzyozbourne <khanosaid726@gmail.com>
2024-03-02 21:24:51 -0500

    java setup done

---
+++ java/stack/lib/src/main/java/org/example/NearestGreaterOrSmaller.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public final class NearestGreaterOrSmaller {
+
+    public static int[] nearestGreaterToRight(int arr[]) {
+
+    };
+
+    public static int[] nearestGreaterToLeft(int arr[]) {
+
+    };
+
+    public static int[] nearestSmallerToRight(int arr[]) {
+
+    };
+
+    public static int[] nearestSmallerToLeft(int arr[]) {
+
+    };
+
+}
--- java/stack/lib/src/main/java/org/example/NearestGreaterToRight.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public final class NearestGreaterToRight {
-    public static void nearestGreaterToRight(int arr[]) {
-
-    };
-}
