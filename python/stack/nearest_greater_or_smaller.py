787f38190a7f05a283e238c0d84c4994ac753942
ozzyozbourne <khanosaid726@gmail.com>
2024-03-02 20:13:04 -0500

    stack 4 done added gitignore

--- python/stack/nearest_greater_or_smaller.py
+++ python/stack/nearest_greater_or_smaller.py
@@ -10,9 +10,63 @@ def nearest_greater_to_the_right(arr):
                 stack.pop()
             if not stack:
                 res.append(-1)
-            else: 
+            else:
                 res.append(stack[-1])
         stack.append(arr[i])
     res.reverse()
     return res
-                
+
+
+def nearest_greater_to_the_left(arr):
+    stack, res = [], []
+    for i in range(len(arr)):
+        if not stack:
+            res.append(-1)
+        elif stack[-1] > arr[i]:
+            res.append(stack[-1])
+        else:
+            while stack and stack[-1] <= arr[i]:
+                stack.pop()
+            if not stack:
+                res.append(-1)
+            else:
+                res.append(stack[-1])
+        stack.append(arr[i])
+    return res
+
+
+def nearest_smaller_to_the_right(arr):
+    stack, res = [], []
+    for i in range(len(arr)-1, -1, -1):
+        if not stack:
+            res.append(-1)
+        elif stack[-1] < arr[i]:
+            res.append(stack[-1])
+        else:
+            while stack and stack[-1] >= arr[i]:
+                stack.pop()
+            if not stack:
+                res.append(-1)
+            else:
+                res.append(stack[-1])
+        stack.append(arr[i])
+    res.reverse()
+    return res
+
+
+def nearest_smaller_to_the_left(arr):
+    stack, res = [], []
+    for i in range(len(arr)):
+        if not stack:
+            res.append(-1)
+        elif stack[-1] < arr[i]:
+            res.append(stack[-1])
+        else:
+            while stack and stack[-1] >= arr[i]:
+                stack.pop()
+            if not stack:
+                res.append(-1)
+            else:
+                res.append(stack[-1])
+        stack.append(arr[i])
+    return res
--- python/stack/test.py
+++ python/stack/test.py
@@ -1,18 +1,50 @@
 import unittest
-from nearest_greater_or_smaller import nearest_greater_to_the_right
+from nearest_greater_or_smaller import (
+        nearest_greater_to_the_right as ngr,
+        nearest_greater_to_the_left as ngl,
+        nearest_smaller_to_the_right as nsr,
+        nearest_smaller_to_the_left as nsl
+        )
+
 
 class Test(unittest.TestCase):
-    
+
     def test_one(self):
-        self.assertEqual([3, 4, 4, -1], nearest_greater_to_the_right([1, 3, 2,4]))
-    
+        self.assertEqual([3, 4, 4, -1], ngr([1, 3, 2, 4]))
+
     def test_two(self):
-        self.assertEqual([-1, -1, -1, -1, -1], nearest_greater_to_the_right([5, 4, 3, 2, 1]))
+        self.assertEqual([-1, -1, -1, -1, -1], ngr([5, 4, 3, 2, 1]))
 
     def test_three(self):
-        self.assertEqual([70, 80, 80, -1, -1], nearest_greater_to_the_right([20, 70, 30, 80, 60]))
+        self.assertEqual([70, 80, 80, -1, -1], ngr([20, 70, 30, 80, 60]))
+
+    def test_four(self):
+        self.assertEqual([-1, -1, 3, -1], ngl([1, 3, 2, 4]))
+
+    def test_five(self):
+        self.assertEqual([-1, 5, 4, 3, 2], ngl([5, 4, 3, 2, 1]))
+
+    def test_six(self):
+        self.assertEqual([-1, -1, 70, -1, 80], ngl([20, 70, 30, 80, 60]))
+
+    def test_seven(self):
+        self.assertEqual([-1, 1, 1, 2], nsl([1, 3, 2, 4]))
+
+    def test_eight(self):
+        self.assertEqual([-1, -1, -1, -1, -1], nsl([5, 4, 3, 2, 1]))
+
+    def test_nine(self):
+        self.assertEqual([-1, 20, 20, 30, 30], nsl([20, 70, 30, 80, 60]))
+
+    def test_ten(self):
+        self.assertEqual([-1, 2, -1, -1], nsr([1, 3, 2, 4]))
+
+    def test_eleven(self):
+        self.assertEqual([4, 3, 2, 1, -1], nsr([5, 4, 3, 2, 1]))
+
+    def test_twelve(self):
+        self.assertEqual([-1, 30, -1, 60, -1], nsr([20, 70, 30, 80, 60]))
+
 
 if __name__ == "__main__":
     unittest.main()
-
-
