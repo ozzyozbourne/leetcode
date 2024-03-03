eb1bdd5048be5255930ccd62eb9c0ba0fed7acc9
ozzyozbourne <khanosaid726@gmail.com>
2024-03-02 23:51:10 -0500

    updated the soln

--- java/stack/lib/src/main/java/org/example/NearestGreaterOrSmaller.java
+++ java/stack/lib/src/main/java/org/example/NearestGreaterOrSmaller.java
@@ -1,9 +1,36 @@
 package org.example;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public final class NearestGreaterOrSmaller {
 
     public static int[] nearestGreaterToRight(int arr[]) {
 
+        final Deque<Integer> stack = new ArrayDeque<>(arr.length);
+        final List<Integer> res = new ArrayList<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (stack.isEmpty())
+                res.add(-1);
+            else if (stack.peek() > arr[i])
+                res.add(stack.peek());
+            else {
+                while (!stack.isEmpty() && stack.peek() <= arr[i])
+                    stack.pop();
+                if (stack.isEmpty())
+                    res.add(-1);
+                else
+                    res.add(stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = res.get(arr.length - 1 - i);
+        return arr;
     };
 
     public static int[] nearestGreaterToLeft(int arr[]) {
