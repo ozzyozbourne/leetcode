28cee1c066e7a3ddb3371c154fd6ea6ff14d802a
ozzyozbourne <khanosaid726@gmail.com>
2024-03-03 22:11:50 -0500

    just one fix

--- java/stack/lib/src/main/java/org/example/NearestGreaterOrSmaller.java
+++ java/stack/lib/src/main/java/org/example/NearestGreaterOrSmaller.java
@@ -4,13 +4,14 @@ import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
+import java.util.Stack;
 
 public final class NearestGreaterOrSmaller {
 
     public static int[] nearestGreaterToRight(int arr[]) {
 
         final Deque<Integer> stack = new ArrayDeque<>(arr.length);
-        final List<Integer> res = new ArrayList<>();
+        final List<Integer> res = new ArrayList<>(arr.length);
 
         for (int i = arr.length - 1; i >= 0; i--) {
             if (stack.isEmpty())
@@ -35,6 +36,27 @@ public final class NearestGreaterOrSmaller {
 
     public static int[] nearestGreaterToLeft(int arr[]) {
 
+        final Deque<Integer> stack = new ArrayDeque<>(arr.length);
+        final List<Integer> res = new ArrayList<>(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.isEmpty())
+                res.add(-1);
+            else if (stack.peek() > arr[i])
+                res.add(stack.peek());
+            else {
+                while (!stack.isEmpty() && stack.peek() <= arr[i])
+                    stack.pop();
+                if (stack.isEmpty())
+                    res.add(-1);
+                else
+                    res.add(stack.peek());
+            }
+            stack.push(arr[i]);
+        }
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = res.get(i);
+        return arr;
     };
 
     public static int[] nearestSmallerToRight(int arr[]) {
