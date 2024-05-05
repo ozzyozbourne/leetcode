2ce718005aabf73808db312421ee32788c0d7207
ozzyozbourne <khanosaid726@gmail.com>
2024-05-05 00:43:03 -0400

    saga continues

--- elixir/misc/lib/misc.ex
+++ elixir/misc/lib/misc.ex
@@ -15,22 +15,76 @@ defmodule Misc do
   def  factorial(n),      do: factorial(n, 1)
   defp factorial(1, acc), do: acc
   defp factorial(n, acc), do: factorial(n-1, n*acc)
-    
-  # LC 2788. Split Strings by Separator
-  @spec split_words_by_separator(words :: [String.t], separator :: char) :: [String.t]
-  def split_words_by_separator(words, separator) do
-    words 
-        |> Enum.map(&(String.split(&1, <<separator>>)))
-        |> Enum.concat()
-        |> Enum.filter(&(&1 != ""))
-  end
-  
-  # LC 2828. Check if a String Is an Acronym of Words
-  @spec is_acronym(words :: [String.t], s :: String.t) :: boolean
-  def is_acronym(words, s),  do: acronym?(words, s)
 
-  defp acronym?([<<ch, _::bytes>> | tl], <<ch1, rest::bytes>>), do: ch == ch1 && acronym?(tl, rest)
-  defp acronym?([], <<>>), do: true
-  defp acronym?(_, _), do: false
-    
+
+  defmodule LC_2788_SplitStringsbySeparator do
+    @spec split_words_by_separator(words :: [String.t], separator :: char) :: [String.t]
+    def split_words_by_separator(words, separator) do
+      words 
+      |> Enum.map(&(String.split(&1, <<separator>>)))
+      |> Enum.concat()
+      |> Enum.filter(&(&1 != ""))
+    end
+  end
+
+
+  defmodule LC_2828_CheckifAStringIsAnAcronymofWords do
+    @spec is_acronym(words :: [String.t], s :: String.t) :: boolean
+    def is_acronym(words, s),  do: acronym?(words, s)
+
+    defp acronym?([<<ch, _::bytes>> | tl], <<ch1, rest::bytes>>), do: ch == ch1 && acronym?(tl, rest)
+    defp acronym?([], <<>>), do: true
+    defp acronym?(_, _), do: false
+  end
+
+
+  defmodule LC_2864_MaximumOddBinaryNumber do
+    @spec maximum_odd_binary_number(s :: String.t) :: String.t
+    def maximum_odd_binary_number(s), do: solve(s, "", "")
+
+    defp solve(<<?0, rest::binary>>, ones, zeros), do: solve(rest, ones, <<?0, zeros::binary>>)
+    defp solve(<<?1, rest::binary>>, ones, zeros), do: solve(rest, <<?1, ones::binary>>, zeros)
+    defp solve("", <<_, ones::binary>>, zeros),    do: ones <> zeros <> <<?1>>
+  end
+
+
+  defmodule LC_2798_NumberofEmployeesWhoMettheTarget do
+    @spec number_of_employees_who_met_target(hours :: [integer], target :: integer) :: integer
+    def number_of_employees_who_met_target(hours, target), do: Enum.count(hours, &(&1 >= target))
+  end
+
+
+  defmodule LC_3005_CountElementsWithMaximumFrequency do
+    @spec max_frequency_elements(nums :: [integer]) :: integer
+    def max_frequency_elements(nums) do
+      Enum.frequencies(nums)
+      |> Map.values()
+      |> Enum.frequencies()
+      |> Enum.max()
+      |> then(fn {k, v} -> k * v end)
+    end
+  end
+
+
+  defmodule LC_58_LengthofLastWord do
+    @spec length_of_last_word(s :: String.t) :: integer
+    def length_of_last_word(s), do: solve(s, true, 0)
+
+    defp solve(<<?\s, tail::binary>>, _, ans), do: solve(tail, true, ans)
+    defp solve(<<_, tail::binary>>, true, _), do: solve(tail, false, 1)
+    defp solve(<<_, tail::binary>>, false, ans), do: solve(tail, false, ans + 1)
+    defp solve("", _, ans), do: ans
+  
+  end
+
+
+  defmodule LC_392_IsSubsequence do
+    @spec is_subsequence(s :: String.t, t :: String.t) :: boolean
+    def is_subsequence("", _), do: true
+    def is_subsequence(_, ""), do: false
+    def is_subsequence(<<ch, s::binary>>, <<ch, t::binary>>), do: is_subsequence(s, t)
+    def is_subsequence(s, <<_, t::binary>>), do: is_subsequence(s, t)
+  end
+
+
 end
