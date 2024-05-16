a565624cb1f1452c4b0457f3b592adc046650595
ozzyozbourne <khanosaid726@gmail.com>
2024-05-16 03:23:03 -0400

    updated

--- rust/stack/src/lib.rs
+++ rust/stack/src/lib.rs
@@ -1,8 +1,32 @@
 #[cfg(test)]
-mod nearest_greater_or_smaller {
+mod ngr {
+
+    use std::collections::VecDeque;
+
+    fn ngr_mono(nums: Vec<i32>) -> Vec<i32> {
+        let (mut st, mut res): (VecDeque<usize>, Vec<i32>) =
+            (VecDeque::new(), vec![-1; nums.len()]);
+        for (index, &value) in nums.iter().enumerate() {
+            while !st.is_empty() && nums[*st.back().unwrap()] < value {
+                res[st.pop_back().unwrap()] = value;
+            }
+            st.push_back(index);
+        }
+        res
+    }
 
     #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
+    fn test_ngr_mono_one() {
+        assert_eq!(ngr_mono(vec![1, 3, 2, 4]), vec![3, 4, 4, -1]);
+    }
+
+    #[test]
+    fn test_ngr_mono_two() {
+        assert_eq!(ngr_mono(vec![5, 4, 3, 2, 1]), vec![-1, -1, -1, -1, -1]);
+    }
+
+    #[test]
+    fn test_ngr_mono_three() {
+        assert_eq!(ngr_mono(vec![20, 70, 30, 80, 60]), vec![70, 80, 80, -1, -1]);
     }
 }
