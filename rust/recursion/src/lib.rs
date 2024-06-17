cb751d85a4f06022c2485b2097a9ebe1d771fc19
ozzyozbourne <khanosaid726@gmail.com>
2024-06-17 04:01:15 -0500

    rust one fix

--- rust/recursion/src/lib.rs
+++ rust/recursion/src/lib.rs
@@ -213,7 +213,7 @@ mod lc_326 {
     }
 
     #[test]
-    fn lc_21_tests() {
+    fn lc_326_tests() {
         struct TestValue {
             input: i32,
             expected: bool,
@@ -237,6 +237,211 @@ mod lc_326 {
         for t in test_cases.into_iter() {
             assert_eq!(is_power_of_three(t.input), t.expected);
             assert_eq!(is_power_of_three_pure_functional(t.input), t.expected);
+        }
+    }
+}
+
+#[cfg(test)]
+mod lc_342 {
+
+    fn is_power_of_four(n: i32) -> bool {
+        fn inner(n: i32, r: i32) -> bool {
+            match (n, r) {
+                (_, r) if r != 0 => false,
+                (n, _) if n < 1 => false,
+                (1, 0) => true,
+                _ => inner(n / 4, n % 4),
+            }
+        }
+        inner(n, 0)
+    }
+
+    #[test]
+    fn lc_342_tests() {
+        struct TestValue {
+            input: i32,
+            expected: bool,
+        }
+
+        let test_cases = [
+            TestValue {
+                input: 16,
+                expected: true,
+            },
+            TestValue {
+                input: 5,
+                expected: false,
+            },
+            TestValue {
+                input: 1,
+                expected: true,
+            },
+        ];
+
+        for t in test_cases.into_iter() {
+            assert_eq!(is_power_of_four(t.input), t.expected);
+        }
+    }
+}
+
+#[cfg(test)]
+mod lc_231 {
+
+    fn is_power_of_two(n: i32) -> bool {
+        fn inner(n: i32, r: i32) -> bool {
+            match (n, r) {
+                (_, r) if r != 0 => false,
+                (n, _) if n < 1 => false,
+                (1, 0) => true,
+                _ => inner(n / 2, n % 2),
+            }
+        }
+        inner(n, 0)
+    }
+
+    #[test]
+    fn lc_231_tests() {
+        struct TestValue {
+            input: i32,
+            expected: bool,
+        }
+
+        let test_cases = [
+            TestValue {
+                input: 1,
+                expected: true,
+            },
+            TestValue {
+                input: 16,
+                expected: true,
+            },
+            TestValue {
+                input: 3,
+                expected: false,
+            },
+        ];
+
+        for t in test_cases.into_iter() {
+            assert_eq!(is_power_of_two(t.input), t.expected);
+        }
+    }
+}
+
+#[cfg(test)]
+mod lc_203 {
+
+    use crate::{listnode_to_vec, vec_to_listnode, ListNode, T};
+
+    fn remove_elements(head: T, val: i32) -> T {
+        //avoid option use senital
+        let mut dummy = Box::new(ListNode {
+            val: -1,
+            next: head,
+        });
+        let mut curr = &mut dummy;
+
+        while let Some(n) = curr.next.as_mut() {
+            if n.val == val {
+                curr.next = n.next.take();
+            } else {
+                curr = curr.next.as_mut().unwrap();
+            }
+        }
+        dummy.next
+    }
+
+    #[test]
+    fn lc_231_tests() {
+        struct TestValue {
+            input1: T,
+            input2: i32,
+            expected: Vec<i32>,
+        }
+
+        let test_cases = [
+            TestValue {
+                input1: vec_to_listnode(vec![1, 2, 6, 3, 4, 5, 6]),
+                input2: 6,
+                expected: vec![1, 2, 3, 4, 5],
+            },
+            TestValue {
+                input1: vec_to_listnode(vec![]),
+                input2: 1,
+                expected: vec![],
+            },
+            TestValue {
+                input1: vec_to_listnode(vec![7, 7, 7, 7]),
+                input2: 7,
+                expected: vec![],
+            },
+        ];
+
+        for t in test_cases.into_iter() {
+            assert_eq!(
+                listnode_to_vec(remove_elements(t.input1, t.input2)),
+                t.expected
+            );
+        }
+    }
+}
+
+#[cfg(test)]
+mod lc_234 {
+
+    use crate::{vec_to_listnode, T};
+
+    fn is_palindrome(head: T) -> bool {
+        if head.is_none() || head.as_ref().unwrap().next.is_none() {
+            return true;
+        }
+        let find_mid = || {
+            let (mut mid, mut fast) = (0, &head);
+            while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
+                fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
+                mid += 1;
+            }
+            mid
+        };
+        let count = find_mid();
+
+        let split = || {
+            let (mut prev, mut cur) = (None, head);
+            for _ in 0..count {
+                if let Some(mut node) = cur {
+                    cur = node.next;
+                    node.next = prev;
+                    prev = Some(node);
+                }
+            }
+            if prev == cur {
+                true
+            } else {
+                prev == cur.unwrap().next
+            }
+        };
+        split()
+    }
+
+    #[test]
+    fn lc_231_tests() {
+        struct TestValue {
+            input: T,
+            expected: bool,
+        }
+
+        let test_cases = [
+            TestValue {
+                input: vec_to_listnode(vec![1, 2, 2, 1]),
+                expected: true,
+            },
+            TestValue {
+                input: vec_to_listnode(vec![1, 2]),
+                expected: false,
+            },
+        ];
+
+        for t in test_cases.into_iter() {
+            assert_eq!(is_palindrome(t.input), t.expected);
         }
     }
 }
