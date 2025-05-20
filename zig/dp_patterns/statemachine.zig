23b0a76fbdddc51b198aacf4ab67d2b09400f57a
ozzyozbourne <khanosaid726@gmail.com>
2025-05-20 09:47:24 -0400

    dp sucks

---
+++ zig/dp_patterns/statemachine.zig
@@ -0,0 +1,153 @@
+/// DP State machine pattern -> https://blog.algomaster.io/p/20-patterns-to-master-dynamic-programming
+const std = @import("std");
+
+fn best_time_to_buy_and_sell_stocks(prices: []const i32) i32 {
+    var profit: i32, var loss: i32 = .{0, -prices[0]};
+    for (1..prices.len) |i| {
+        const prev_profit: i32, const prev_loss: i32 = .{profit, loss};
+        loss = @max(prev_loss, -prices[i]);
+        profit = @max(prev_profit, prev_loss + prices[i]);
+    }
+    return profit;
+}
+
+test "lc_121" {
+    const testcases = [_]struct {
+        prices: []const i32,
+        expected: i32
+    } {
+        .{
+            .prices = &.{7, 1, 5, 3, 6, 4},
+            .expected = 5
+        }, 
+        .{
+            .prices = &.{7, 6, 4, 3, 1},
+            .expected = 0
+        }
+    };
+
+    for (testcases) |tc| {
+        try std.testing.expectEqual(tc.expected, best_time_to_buy_and_sell_stocks(tc.prices));
+    }
+}
+
+fn best_time_to_buy_and_sell_stocks_ii(prices: []const i32) i32{
+    var profit: i32, var loss: i32 = .{0, -prices[0]};
+    for (1..prices.len) |i| {
+        const prev_profit: i32, const prev_loss: i32 = .{profit, loss};
+        loss = @max(prev_loss, prev_profit - prices[i]);
+        profit = @max(prev_profit, prev_loss + prices[i]);
+    }
+    return profit;
+}
+
+test "lc_122" {
+    const testcases = [_]struct {
+        prices: []const i32,
+        expected: i32
+    } {
+        .{
+            .prices = &.{7, 1, 5, 3, 6, 4},
+            .expected = 7
+        }, 
+        .{
+            .prices = &.{7, 6, 4, 3, 1},
+            .expected = 0
+        },
+        .{
+            .prices = &.{1, 2, 3, 4 ,5},
+            .expected = 4
+        }
+    };
+
+    for (testcases) |tc| {
+        try std.testing.expectEqual(tc.expected, best_time_to_buy_and_sell_stocks_ii(tc.prices));
+    }
+}
+
+fn best_time_to_buy_and_sell_stocks_iii(prices: []const i32) i32 {
+    var p1: i32, var l1: i32, var p2: i32, var l2: i32 = .{0, -prices[0], 0, std.math.minInt(i32)};
+    for (1..prices.len) |i| {
+        const prev_p1: i32, const prev_l1: i32, const prev_p2: i32, const prev_l2:i32 = .{p1, l1, p2, l2};
+            l1 = @max(prev_l1, -prices[i]);
+            p1 = @max(prev_p1, prev_l1 + prices[i]);
+
+            l2 = @max(prev_l2, prev_l1 - prices[i]);
+            p2 = @max(prev_p2, prev_l2 + prices[i]);
+    }
+    return @max(p1, p2);
+}
+
+
+test "lc_123" {
+    const testcases = [_]struct {
+        prices: []const i32,
+        expected: i32
+    } {
+        .{
+            .prices = &.{3, 3, 5, 0, 0, 3, 1, 4},
+            .expected = 4
+        }, 
+        .{
+            .prices = &.{7, 6, 4, 3, 1},
+            .expected = 0
+        },
+        .{
+            .prices = &.{1, 2, 3, 4 ,5},
+            .expected = 4
+        }
+    };
+
+    for (testcases) |tc| {
+        try std.testing.expectEqual(tc.expected, best_time_to_buy_and_sell_stocks_iii(tc.prices));
+    }
+}
+
+fn best_time_to_buy_and_sell_stocks_iv(gpa: std.mem.Allocator, prices: []const i32, k: i32) i32 {
+    if (@as(usize, @intCast(k)) > prices.len) {
+        var sum: i32 = 0;
+        for (1..prices.len) |i| {
+            sum += @max(0, prices[i] - prices[i-1]);
+        }
+        return sum;
+    }
+    var buy = gpa.alloc(i32, @intCast(k + 1)) catch unreachable;
+    var sell = gpa.alloc(i32, @intCast(k + 1)) catch unreachable;
+    defer {    
+        gpa.free(buy);
+        gpa.free(sell);
+    }
+    @memset(buy, std.math.minInt(i32));
+    @memset(sell, 0);
+    for (prices) |price| {
+        for (1..@intCast(k+1)) |i| {
+            buy[i] = @max(buy[i], sell[i - 1] - price);
+            sell[i] = @max(sell[i], buy[i] + price);
+        }
+    }
+    return sell[@intCast(k)];
+}
+
+test "lc_188" {
+    const testcases = [_]struct {
+        prices: []const i32,
+        k: i32,
+        expected: i32
+    } {
+        .{
+            .prices = &.{3, 2, 6, 5, 0, 3},
+            .k = 2,
+            .expected = 7
+        }, 
+        .{
+            .prices = &.{2, 4, 1},
+            .k = 2, 
+            .expected = 2
+        }
+    };
+
+    for (testcases) |tc| {
+        try std.testing.expectEqual(tc.expected, best_time_to_buy_and_sell_stocks_iv(std.testing.allocator, tc.prices, tc.k));
+    }
+}
+
